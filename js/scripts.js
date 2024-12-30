39f793160d45636ae775a80d88db1691e15ae378
TheVuGl <nguyenthevu13052004@gmail.com>
2024-12-30 08:08:05 +0700

    Tuan2Buoi1

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Hàm thay đổi nội dung của đoạn văn bản
+function changeText() {
+  const demoElement = document.getElementById("demo");
+  demoElement.textContent = "You clicked the button!";
+  demoElement.style.color = "red"; // Thay đổi màu chữ thành đỏ
+}
